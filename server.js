838ed861af7e770bc863345bcf09431a73e149e4
lester <lester@Charless-iMac.home>
2017-12-01 03:33:48 -0500

    Updated port

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express')
-// process.env.PORT ||
-var port     = 3000;
+var port     = process.env.PORT || 80;
 var pug = require('pug')
 var axios = require('axios')
 var bodyParser = require('body-parser')
