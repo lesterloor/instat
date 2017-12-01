5b41ba42105eeb3affa1041b9d9880a0d7f3d975
lester <lester@Charless-iMac.home>
2017-12-01 01:57:04 -0500

    Simple server set up with working pug template

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express')
+// process.env.PORT ||
+var port     = 8000;
+var pug = require('pug')
+var bodyParser = require('body-parser')
+var app = express()
+
+
+
+
+
+
+///ROUTES ////
+require('./app/routes.js')(app); // load our routes and pass in our app and fully configured passport
+
+
+
+///MIDDLEWARE ////
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set(express.static('./public'));
+
+/////////////////
+
+app.listen(port);
+console.log('The magic happens on port ' + port);
