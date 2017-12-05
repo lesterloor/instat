e048697185761cbbe20480e0aee0613d5a0b88e0
lester <lester@Stevens-iMac-2.local>
2017-12-05 15:11:31 -0500

    extra feature

--- app/routes.js
+++ app/routes.js
@@ -32,29 +32,24 @@ module.exports = function(app) {
     }).catch(err =>{
       console.log(err);
       res.render('index.pug',{notfound:`Sorry username does not exist`})
-
     })
-    // var accessToken = "5724115162.7b3b2a3.97b6d6e6d0164830845fe2568e967da7"
-    // axios.get(`https://api.instagram.com/v1/users/self/?access_token=${accessToken}`)
-    //   .then(function (response) {
-    //     axios.get(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${accessToken}&count=3`)
-    //       .then(function (recentMedia) {
-    //         // console.log(recentMedia.data.data);
-    //         var results = response.data.data
-    //         var recentMedia = recentMedia.data.data
-    //         // console.log(response.data.data);
-    //         res.render('results.pug',{searchData:results,recentMedia: recentMedia});
-    //       })
-    //       .catch(function (error) {
-    //         console.log(error);
-    //       });
-    //     // console.log(response.data.data);
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //   });
+  })
+  app.get('/prices', function(req, res) {
+    console.log("User to search",req.body.search);
 
-
+    instagramUser(req.body.search).then(userInfo => {
+      const username = userInfo.username
+        ipp.medium(username).then(userProfilePic => {
+            const alldata = [userInfo,userProfilePic]
+            console.log(alldata[0].username);
+            console.log(alldata[1]);
+            res.render('results.pug',{alldata:alldata})
+          });
+        //=> {description: 'A wonderful description', email: 'unicorns@foo.com', ...}
+    }).catch(err =>{
+      console.log(err);
+      res.render('index.pug',{notfound:`Sorry username does not exist`})
+    })
   })
 
 
